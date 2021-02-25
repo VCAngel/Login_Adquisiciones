e4865ec6bc639df22a2508ed8b30a7d246d74960
VCAngel <vargascasavantesangel.329@gmail.com>
2021-02-25 06:42:50 -0700

    Header terminado con JS implementado; revisar TODOs

--- js/util/navbar-control.js
+++ js/util/navbar-control.js
@@ -1,4 +1,31 @@
 //* Script para controlar el navbar del header
-let navbarTitle = document.getElementById("navbarTitle");
+(function () {
+    let navArray = document.querySelectorAll(".nav-link");
+    let navbarTitle = document.getElementById("navbarTitle");
+    let wdwWidth = window.innerWidth; // *Ancho de la ventana interna
 
-// TODO Realizar el Script
+    setInterval(function () {
+        //console.log(wdwWidth);
+        if (wdwWidth < 992) { // ? Para controlar el tamaño del menú colapsable
+            navArray.forEach(link => {
+                link.style.fontSize = "80%";
+                link.style.fontWeight = "400";
+            });
+            navbarTitle.innerHTML = "Menú";
+            updateWdwWidth();
+        } else {
+            navArray.forEach(link => {
+                link.style.fontSize = "100%";
+                link.style.fontWeight = "700";
+            });
+            navbarTitle.innerHTML = "";
+            updateWdwWidth();
+        }
+    }, 200);
+
+    function updateWdwWidth() { // *Actualizar el valor del ancho
+        wdwWidth = window.innerWidth;
+    }
+
+})();
+
