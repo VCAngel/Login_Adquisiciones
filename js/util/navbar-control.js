c2e89b1d633bbe2192045e617df9fe4c9a5a43b6
VCAngel <vargascasavantesangel.329@gmail.com>
2021-02-26 02:32:26 -0700

    Header test; falta resto de form

--- js/util/navbar-control.js
+++ js/util/navbar-control.js
@@ -21,7 +21,7 @@
             navbarTitle.innerHTML = "";
             updateWdwWidth();
         }
-    }, 200);
+    }, 100);
 
     function updateWdwWidth() { // *Actualizar el valor del ancho
         wdwWidth = window.innerWidth;
